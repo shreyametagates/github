9ac4f21c184c00351abd2a5486e24a69bb2d935c
shreyametagates <shreya.metagates@gmail.com>
2023-07-25 12:18:45 +0530

    adding msg in sum.js

--- sum.js
+++ sum.js
@@ -1,11 +1,11 @@
-function factorial(n) {
+function fact(n) {
     if (n === 0 || n === 1) {
       return 1;
     } else {
-      return n * factorial(n - 1);
+      return n * fact(n - 1);
     }
   }
   
   const number = 5;
-  const result = factorial(number);
-  console.log(`The factorial of ${number} is: ${result}`);
+  const result = fact(number);
+  console.log(`The fact of ${number} is: ${result}`);
