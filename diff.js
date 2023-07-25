5e19aece58d59673e9f1410aa393a7efb8815c0d
shreyametagates <shreya.metagates@gmail.com>
2023-07-25 12:16:56 +0530

    initial commit

---
+++ diff.js
@@ -0,0 +1,9 @@
+// Define two numbers
+let num1 = 10;
+let num2 = 5;
+
+// Find the difference
+let difference = num1 - num2;
+
+// Output the result
+console.log("The difference is: " + difference);
